07e660ad0ee85bfc6bbcc908aacada5fa510acec
Enrique Jimenez <ejimenez2292@gmail.com>
2022-04-10 18:44:02 -0500

    added questions for engineer and intern

--- index.js
+++ index.js
@@ -5,72 +5,81 @@ const Engineer = require('./lib/Engineer')
 const Inter = require('./lib/Intern')
 const Manager = require('./lib/Manager')
 
-const employeeQuestions = [{
-    type: 'input',
-    name: 'name',
-    message: "What is your employees name?"
-},
-{
-    type: 'input',
-    name: 'id',
-    message: 'What is your employee ID?'
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'What is your employees email?'
-}, 
-{
-    type: 'checkbox',
-    name: 'role',
-    message: 'What is your employees role?',
-    choices: ['Manager', 'Engineer', 'Intern'], 
-    validate: managerInput => {
-        if ('Manager') {
-            return managerQ
-        } else {
-           return employeeData
-        }
-    }
-}]
+const employees = [];
 
-const managerQ = [{
-    type: 'input',
-    name: 'office',
-    message: 'Please enter your office number.'
-},
-{
-    type: 'confirm',
-    name: 'add',
-    message: 'Would you like to add another employee?',
-    default: false,
-    validate: confirmation => {
-        if (confirmation === true) {
-            return {
-                type: 'checkbox',
-                name: 'role',
-                message: 'What is your employees role?',
-                choices: ['Manager', 'Engineer', 'Intern'],
-                validate: engineerInput => {
-                    if ('Engineer') {
-                        return engineerQ()
-                    } else {
-                        return employeeData
-                    }
-                }
-            }
-            
-        } else {
-            return employeeData
+function managerQuestions() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the Manager name?',
+            name: `mgrName`
+        },
+        {
+            type: 'input',
+            message: 'What is the Manager ID?',
+            name: `mgrId`
+        },
+        {
+            type: 'input',
+            message: 'What is the Manager Email?',
+            name: `mgrEmail`
+        },
+        {
+            type: 'input',
+            message: 'What is the Manager office Number?',
+            name: `mgrOffice`
         }
-    }
-}]
-
-function init() {
-    inquirer.prompt(employeeQuestions)
-    .then(function(data) {
-        writeToFile("index.html", generateHTML(data));
+    ]).then(response => {
+        const newMgr = new Manager(response.mgrName, response.mgrId, response.mgrEmail, response.mgrOffice);
+        console.log(newMgr.getRole())
+        employees.push(newMgr)
+        mainMenu()
     })
 }
 
-init()
+function mainMenu () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add more?',
+            name: `choice`,
+            choices: ["Engineer", "Intern", "I am done"]
+        }
+    ]).then(response => {
+        if (response.choice == "Engineer") {
+            engineerQuestions()
+        } else if (response.choice == "Intern") {
+            internQuestions()
+        } else {
+            createHTML()
+        }
+    })
+}
+
+function engineerQuestions() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your github link?',
+            name: 'github'
+        }
+    ]).then (response => {
+        if (response == 'githubInput')
+            return mainMenu()
+    })
+}
+
+function internQuestions() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please add employees school',
+            name: 'school'
+        }
+    ]).then (response => {
+        if (response == 'schoolInput')
+            return mainMenu()
+    })
+}
+
+managerQuestions();
